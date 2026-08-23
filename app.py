250bd50a26132af407a57a5d4aed51fac496a0b1
rustamov502 <rdiyorbek069@gmail.com>
2026-08-23 14:20:58 +0500

    Update Flask app configuration for deployment
    Refactor Flask app to use environment variable for port and set host to 0.0.0.0.

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+import os
 from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return send_from_directory('.', 'index.html')
 
-if __name__ == '__main__':
-    print("🚀 CS 2 Market sayti ishga tushdi: http://127.0.0.1:5000")
-    app.run(debug=True, port=5000)
+@app.route("/")
+def home():
+  return send_from_directory(".", "index.html")
+
+
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host="0.0.0.0", port=port)
